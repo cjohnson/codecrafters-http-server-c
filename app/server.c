04720f82ac65889fa3e478f492bc2499d8d324f9
cjohnson <collin.g.johnson@gmail.com>
2024-07-13 12:49:44 -0400

    Cast to uint

--- app/server.c
+++ app/server.c
@@ -49,7 +49,7 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 
-	int fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int fd = accept(server_fd, (struct sockaddr *) &client_addr, (unsigned int *)&client_addr_len);
 	printf("Client connected\n");
 
 	char *ok = "HTTP/1.1 200 OK\r\n\r\n";
